c39929c4d566c0611fdf08f3336b9a110a48a203
vitdias <55774398+vitdias@users.noreply.github.com>
2024-07-31 23:20:17 -0300

    2nd challenge, files update

--- create_tables.sql
+++ create_tables.sql
@@ -1,2 +1,42 @@
-CREATE TABLE tst AS SELECT;
-    SELECT * from xxx;
+CREATE TABLE user_mart (
+    user_id INT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    account_status VARCHAR(255),
+    birthday_date DATE
+);
+
+CREATE TABLE order_mart (
+    order_id INT PRIMARY KEY,
+    checkout_id INT,
+    buyer_id INT,
+    seller_id INT,
+    order_create_date DATE,
+    order_gross_value DECIMAL(10,2),
+    FOREIGN KEY (buyer_id) REFERENCES user_mart(user_id),
+    FOREIGN KEY (seller_id) REFERENCES user_mart(user_id)
+);
+
+CREATE TABLE item_mart (
+    item_id INT PRIMARY KEY,
+    order_id INT,
+    item_cogs DECIMAL(10,2),
+    category_l1 VARCHAR(255),
+    category_l2 VARCHAR(255),
+    category_l3 VARCHAR(255),
+    FOREIGN KEY (order_id) REFERENCES order_mart(order_id)
+);
+
+CREATE TABLE listings_mart (
+    listing_id INT,
+    item_id INT,
+    item_price DECIMAL(10,2),
+    category_l1 VARCHAR(255),
+    category_l2 VARCHAR(255),
+    category_l3 VARCHAR(255),
+    seller_id INT,
+    listings_status VARCHAR(255),
+    PRIMARY KEY (listing_id, item_id),
+    FOREIGN KEY (item_id) REFERENCES item_mart(item_id),
+    FOREIGN KEY (seller_id) REFERENCES user_mart(user_id)
+);
--- responses_business.sql
+++ responses_business.sql
@@ -52,3 +52,38 @@ WITH prepare_sellers AS (
 -- 3) A new table is requested to be populated with the price and status of the Items at the end of the day. Keep in mind that it must be reprocessable . 
 -- It is worth noting that in the Item table, we will only have the last status reported by the defined PK. (It can be resolved through StoredProcedure) 
 
+CREATE TABLE daily_item_status (
+    item_id INT,
+    daily_price DECIMAL(10,2),
+    daily_status VARCHAR(255),
+    record_date DATE,
+    PRIMARY KEY (item_id, record_date),
+    FOREIGN KEY (item_id) REFERENCES item_mart(item_id)
+);
+
+
+DELIMITER $$
+
+CREATE PROCEDURE PopulateDailyItemStatus()
+BEGIN
+    DELETE FROM daily_item_status WHERE record_date = CURDATE();
+
+    INSERT INTO daily_item_status (item_id, daily_price, daily_status, record_date)
+    SELECT 
+        lm.item_id,
+        lm.item_price,
+        lm.listings_status,
+        CURDATE()
+    FROM 
+        listings_mart lm
+    WHERE
+        lm.listings_status <> 'inactive'; 
+END$$
+
+DELIMITER ;
+
+
+CREATE EVENT IF NOT EXISTS ev_populate_daily_status
+ON SCHEDULE EVERY 1 DAY STARTS CONCAT(CURDATE(), ' 23:59:00')
+DO
+    CALL PopulateDailyItemStatus();
